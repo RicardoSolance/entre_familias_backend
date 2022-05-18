f04a9718dcb7b66a1837de62d076ea3b6640fd99
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-18 17:38:39 +0200

    added login system

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -9,8 +9,42 @@ const getAllUsers = async(req,res) =>{
     }
 }
 
+const signUpUser = async(req,res)=>{
+    try {
+        await user_model.signUpUser(req.body);
+        res.status(200).json("User created succedfully");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updateUser = async(req,res) =>{
+    try {
+        const userBody = {
+            name: req.params.name,
+            body: req.body
+        }
+        await user_model.updateUser(userBody);
+        res.status(200).json("User updated");
+    } catch (error) {
+        res.status(400).json({message: "an error ocurred"})
+    }
+}
+
+const deleteUser = async(req,res)=>{
+    try {
+        await user_model.deleteUser(req.params.email);
+        res.status(200).json("User deleted");
+    } catch (error) {
+        res.status(400).json({message: "an error ocurred"})
+    }
+}
+
 const obj = {
-    getAllUsers
+    getAllUsers,
+    signUpUser,
+    updateUser,
+    deleteUser
 }
 
 module.exports = obj;
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,5 +2,8 @@ const user_controller = require("../controllers/users_controllers");
 const routes = require("express").Router();
 
 routes.get("/users", user_controller.getAllUsers);
+routes.post("/users/create", user_controller.signUpUser);
+routes.put("users/edit/:name", user_controller.updateUser);
+routes.delete("users/delete/:email",user_controller.deleteUser);
 
 module.exports = routes;
