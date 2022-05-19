e6bbbf9926c1d5257bb73a3817452c17199b5779
alenriquez96 <alberto.enriquez96@gmail.com>
2022-05-19 19:10:37 +0200

    add

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 const user_routes = require("./routes/user_routes");
 
+
+app.use(express.urlencoded({extended:true}));//Estas dos son para los métodos put y post, para que el servidor pueda leer la información nueva que le mandamos
+app.use(express.json());
+
+
 app.use("/api",user_routes);
 
 
